bf6d861af9ae2056da51fe2c692d26bf1e943d04
cabicho <jodicabicho@hotmail.com>
2022-07-07 01:46:24 +0200

    providers token

--- main.tf
+++ main.tf
@@ -8,27 +8,29 @@ terraform {
     }
   }
 }
+
 provider "digitalocean" {
+  #https://cloud.digitalocean.com/account/api/tokens
   token = var.do_token
-#"dop_v1_4a99b50bbe2df570243add9fe19f873fb44f5ccb4b193cb8a7f8ffadcb013842" 
+  #"dop_v1_4a99b50bbe2df570243add9fe19f873fb44f5ccb4b193cb8a7f8ffadcb013842" 
 }
+
 # iniciando a declaracao da infraestrutura
 
 resource "digitalocean_kubernetes_cluster" "foo" {
   name   = var.k8s_name 
-#"foo"
+  #"foo"
   region = var.region
-#"nyc1"
+  #"nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.22.8-do.1"
 
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
-    node_count = 3
-
-    
+    node_count = 3    
   }
+	
 }
 
 variable "do_token" {}
